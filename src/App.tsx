d618285b04753cf44cc2cb046fd6c7647de34241
Lucas Alves Pinheiro <l.pinheiro.w@gmail.com>
2025-09-09 11:00:57 -0300

    feat: adiciona rotas e gerenciamento de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import Home from "./pages/Home";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<>
+			<BrowserRouter>
 				<Navbar />
 				<div className="min-h-[80vh] bg-(--secondary-ex-light)">
-						<Home />
+					<Routes>
+						<Route path="/" element={<Home />} />
+					</Routes>
 				</div>
 				<Footer />
+			</BrowserRouter>
 		</>
 	);
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
 		<>
 			<div className="w-full flex justify-center py-4 bg-(--primary) text-white">
 				<div className="container flex justify-between text-lg">
-					<h1 className="font-bold">CuraFarma</h1>
-					<div className="flex gap-4">Lista de Categorias Cadastrar Categorias</div>
+					<Link to="/">
+						<h1 className="font-bold">CuraFarma</h1>
+					</Link>
+					<div className="flex gap-4">
+						<Link to="/categorias"> Lista de Categorias </Link>
+						<Link to="/categorias/cadastro"> Cadastrar Categorias </Link>
+					</div>
 				</div>
 			</div>
 		</>
